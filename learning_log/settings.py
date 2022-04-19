22d7263bb753bb9d37693eed8aeef4a947007979
Nomnivore <6979410+Nomnivore@users.noreply.github.com>
2022-04-19 17:43:58 -0400

    add custom error pages

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -30,7 +30,6 @@ if os.environ.get('DEBUG') == "FALSE":
 else:
     DEBUG = True
 
-
 ALLOWED_HOSTS = []
 
 
@@ -68,7 +67,7 @@ ROOT_URLCONF = 'learning_log.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
